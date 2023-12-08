61db42749193576207a5b97c4a45bb55c1ce6c70
Philip Axelsson <philipaxelaxelsson@gmail.com>
2023-12-08 10:20:32 +0100

    first backend commit

---
+++ routes/workouts.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+
+
+const router = express.Router()
+const {createWorkout,
+       getWorkouts,
+       getWorkout,
+       deleteWorkout,
+       updateWorkout
+    } = require('../controllers/workoutController')
+
+// GET all workouts
+router.get('/', getWorkouts)
+
+// GET a single workout
+router.get('/:id', getWorkout)
+
+// POST a new workout
+router.post('/', createWorkout)
+
+// DELETE a new workout
+router.delete('/:id', deleteWorkout)
+
+// UPDATE a workout
+router.patch('/:id', updateWorkout)
+
+module.exports = router
